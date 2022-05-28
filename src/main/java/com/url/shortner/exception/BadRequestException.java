2831085b57eef6fdc8fed5462209784bd8c85f3b
thsc47 <thsc47@gmail.com>
2022-05-28 20:13:34 -0300

    feat: add custom exceptions

--- src/main/java/com/url/shortner/exception/BadRequestException.java
+++ src/main/java/com/url/shortner/exception/BadRequestException.java
@@ -1,2 +1,10 @@
-package com.url.shortner.exception;public class BadRequestException {
+package com.url.shortner.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String msg){
+        super(msg);
+    }
 }
--- src/main/java/com/url/shortner/exception/NonExistentEntityException.java
+++ src/main/java/com/url/shortner/exception/NonExistentEntityException.java
@@ -1,2 +1,10 @@
-package com.url.shortner.exception;public class NonExistentEntityException {
+package com.url.shortner.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class NonExistentEntityException extends RuntimeException{
+     public NonExistentEntityException(){
+         super("Not Found");
+     }
 }
