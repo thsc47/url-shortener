4b2fa10110d0be13d3cd501f369eef06083b5a50
thsc47 <thsc47@gmail.com>
2022-05-28 17:13:08 -0300

    feat: add shortner mapper

--- src/main/java/com/url/shortner/mapper/ShortUrlMapper.java
+++ src/main/java/com/url/shortner/mapper/ShortUrlMapper.java
@@ -1,2 +1,12 @@
-package com.url.shortner.mapper;public class ShortUrlMapper {
+package com.url.shortner.mapper;
+
+import com.url.shortner.domain.ShortUrl;
+
+public class ShortUrlMapper {
+
+    public static ShortUrl toDomain(String url){
+        return ShortUrl.builder()
+                .url(url)
+                .build();
+    }
 }
