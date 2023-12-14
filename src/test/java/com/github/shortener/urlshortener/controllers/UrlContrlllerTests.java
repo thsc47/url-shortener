982e93a8f99b26cc71f965d169e11972e4202d76
thsc47 <thsc47@gmail.com>
2023-12-14 20:42:54 -0300

    test: add `UrlController` scaffold

---
+++ src/test/java/com/github/shortener/urlshortener/controllers/UrlContrlllerTests.java
@@ -0,0 +1,8 @@
+package com.github.shortener.urlshortener.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UrlContrlllerTests {
+}
