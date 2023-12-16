405826f11fd87eaf0359b47c66ca8f5ef953046a
thsc47 <thsc47@gmail.com>
2023-12-16 16:52:38 -0300

    refactor: change Controller annotation on `UrlController`

--- src/main/java/com/github/shortener/urlshortener/controllers/UrlController.java
+++ src/main/java/com/github/shortener/urlshortener/controllers/UrlController.java
@@ -4,14 +4,11 @@ import com.github.shortener.urlshortener.domains.Url;
 import com.github.shortener.urlshortener.services.UrlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import static org.springframework.http.HttpStatus.CREATED;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/url")
 public class UrlController {
@@ -20,9 +17,8 @@ public class UrlController {
 
     @PostMapping("/short")
     public ResponseEntity<Url> shortAnUrl(@RequestParam String url) {
-        return Optional.of(urlService.shortUrl(url))
-                .map(ResponseEntity::ok)
-                .orElseThrow(RuntimeException::new);
+        return ResponseEntity.status(CREATED)
+                .body(urlService.shortUrl(url));
     }
 
 }
