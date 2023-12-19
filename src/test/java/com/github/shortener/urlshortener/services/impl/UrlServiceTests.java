49b014b2e3186db7f733e76a674945749a19f8a9
thsc47 <thsc47@gmail.com>
2023-12-19 18:51:14 -0300

    test: add throw test to `UrlServiceTests`

--- src/test/java/com/github/shortener/urlshortener/services/impl/UrlServiceTests.java
+++ src/test/java/com/github/shortener/urlshortener/services/impl/UrlServiceTests.java
@@ -1,13 +1,11 @@
 package com.github.shortener.urlshortener.services.impl;
 
 import com.github.shortener.urlshortener.domains.Url;
-import com.github.shortener.urlshortener.mappers.UrlMapper;
+import com.github.shortener.urlshortener.exceptions.errors.StandardUrlAlreadyExistsException;
 import com.github.shortener.urlshortener.mappers.UrlMapperImpl;
 import com.github.shortener.urlshortener.repositories.UrlRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
@@ -15,18 +13,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @Import(UrlMapperImpl.class)
 class UrlServiceTests {
 
-    private static String URL_MOCK = "http://test";
-    private static String SHORT_URL_MOCK = "http://test";
+    private static final String URL_MOCK = "http://test";
+    private static final String SHORT_URL_MOCK = "http://test";
 
     @SpyBean
     private UrlServiceImpl urlService;
@@ -49,6 +45,19 @@ class UrlServiceTests {
         assertEquals(URL_MOCK, result.getOriginalUrl());
 
         verify(urlRepository).save(any());
+        verify(urlRepository).findByOriginalUrl(any());
+    }
+
+    @Test
+    void shouldNotRegisterAShortUrlWheOriginalUrlAlreadyHasAShortVersion() {
+        Url urlMock = new Url(1L, URL_MOCK, SHORT_URL_MOCK);
+        when(urlRepository.findByOriginalUrl(anyString()))
+                .thenReturn(Optional.of(urlMock));
+
+        assertThrows(StandardUrlAlreadyExistsException.class,
+                () -> urlService.shortUrl(URL_MOCK));
+
+        verify(urlRepository, never()).save(any());
         verify(urlRepository).findByOriginalUrl(any());
     }
 }
