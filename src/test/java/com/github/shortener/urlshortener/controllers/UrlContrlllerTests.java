c9138d3adccbba4f3fdb13f2516d338546f51f8d
thsc47 <thsc47@gmail.com>
2023-12-20 21:13:15 -0300

    test: add `UrlControllerTests`

--- src/test/java/com/github/shortener/urlshortener/controllers/UrlContrlllerTests.java
+++
@@ -1,8 +0,0 @@
-package com.github.shortener.urlshortener.controllers;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class UrlContrlllerTests {
-}
---
+++ src/test/java/com/github/shortener/urlshortener/controllers/UrlControllerTests.java
@@ -0,0 +1,43 @@
+package com.github.shortener.urlshortener.controllers;
+
+import com.github.shortener.urlshortener.domains.Url;
+import com.github.shortener.urlshortener.services.UrlService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UrlController.class)
+class UrlControllerTests {
+
+    private final static String URL_MOCK = "/url/short";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UrlService urlService;
+
+    @Test
+    void shouldReturnTheCorrectlyShortUrl() throws Exception {
+        when(urlService.shortUrl(anyString()))
+                .thenReturn(urlMock());
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URL_MOCK)
+                        .param("url", "TEST"))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    private Url urlMock() {
+        return new Url(1L, "orginalUrl", "shortUrl");
+    }
+}
